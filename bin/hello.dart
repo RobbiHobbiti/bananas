6dca8664f245fde018e8bbd442246599ed6da81d
RobbiHobbiti <Robbihobbiti@gmail.com>
2025-04-09 20:41:03 +0000

    not done

--- bin/hello.dart
+++ bin/hello.dart
@@ -1,3 +1,22 @@
+import'dart:io';
+
 void main() {
+  print("how tall is your Rectangle?");
+  String hight = stdin.readLineSync().toString();
+  print("how wide is your Rectangle?");
+  String width = stdin.readLineSync().toString();
+
+
+}
+
+class Rectangle {
+  int hight;
+  int width;
+
+  Rectangle ({required this.hight, required this.width});
+
+  void area(){
+
+  }
 
 }
